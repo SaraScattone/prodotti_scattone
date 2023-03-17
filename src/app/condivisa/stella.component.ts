64d05886426922182a8c433c3a24876d642aa8db
SaraScattone <105594848+SaraScattone@users.noreply.github.com>
2023-03-17 11:07:30 +0100

    2 files modified

--- src/app/condivisa/stella.component.ts
+++ src/app/condivisa/stella.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'stella',
   templateUrl: './stella.component.html',
   styleUrls: ['./stella.component.css'],
 })
-export class StellaComponent {
-  rating: number = 4;
+export class StellaComponent implements OnChanges{
+  //@Input() ci permette di prendere il valore dal componente padre
+  @Input() rating: number;
   cropWidth: number = 75;
+
+  ngOnChanges(): void {
+
+    this.cropWidth = this.rating * (75 / 5);
+  }
 }
