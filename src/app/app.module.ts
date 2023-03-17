96e972811cd3efc3723793e36cf2de407fe81ffb
SaraScattone <105594848+SaraScattone@users.noreply.github.com>
2023-03-17 10:18:35 +0100

    2 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ProdottiComponent } from './prodotti/prodotti.component';
+import { ConvertToSpacePipe } from './condivisa/convert-to-space.pipe';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, ProdottiComponent],
+  declarations: [AppComponent, ProdottiComponent, ConvertToSpacePipe],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
